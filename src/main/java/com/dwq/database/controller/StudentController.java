8b5b1c6d6e64946311e396fc5c4d7e28e262b210
beer <beerdwq95@gmail.com>
2018-11-30 16:45:21 +0800

    Initial commit

---
+++ src/main/java/com/dwq/database/controller/StudentController.java
@@ -0,0 +1,18 @@
+package com.dwq.database.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/****************************************************
+ *
+ * @Description:
+ *
+ * @Author: DONGWENQI
+ * @Date: Created in 3:33 PM 2018/11/30
+ * @Modified By:
+ ****************************************************/
+@RestController
+@RequestMapping(value = "/api")
+public class StudentController {
+
+}
---
+++ src/test/java/com/dwq/database/TestRedis.java
@@ -0,0 +1,50 @@
+package com.dwq.database;
+
+import com.dwq.database.modle.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.swing.*;
+
+/****************************************************
+ *
+ * @Description:
+ *
+ * @Author: DONGWENQI
+ * @Date: Created in 3:43 PM 2018/11/30
+ * @Modified By:
+ ****************************************************/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestRedis {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void redisTest() {
+        // redis存储数据
+        String key = "name";
+        redisTemplate.opsForValue().set(key, "yukong");
+        // 获取数据
+        String value = (String) redisTemplate.opsForValue().get(key);
+        System.out.println("获取缓存中key为" + key + "的值为：" + value);
+
+        User user = new User();
+        user.setUsername("yukong");
+        user.setSex(18);
+        user.setId(1L);
+        String userKey = "yukong";
+        redisTemplate.opsForValue().set(userKey, user);
+        User newUser = (User) redisTemplate.opsForValue().get(userKey);
+        System.out.println("获取缓存中key为" + userKey + "的值为：" + newUser);
+
+    }
+
+}
+
